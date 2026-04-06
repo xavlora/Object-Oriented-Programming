04a67df8d1746398fdeb49b71218ced76d2cbc7b
Muhammad Hafiz <7muhammadhafiz@gmail.com>
2026-04-06 11:44:14 +0700

    Xavlorakeren

---
+++ PBO_2511082013/src/main/java/hafiz06042026/MembacaBilangan.java
@@ -0,0 +1,46 @@
+package hafiz06042026;
+
+import java.util.Scanner;
+
+public class MembacaBilangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah bilangan: "); int bilangan = sc.nextInt();
+        switch(bilangan) {
+            case 1:
+                System.out.println("Satu");
+                break;
+            case 2:
+                System.out.println("Dua");
+                break;
+            case 3:
+                System.out.println("Tiga");
+                break;
+            case 4:
+                System.out.println("Empat");
+                break;
+            case 5:
+                System.out.println("Lima");
+                break;
+            case 6:
+                System.out.println("Enam");
+                break;
+            case 7:
+                System.out.println("Tujuh");
+                break;
+            case 8:
+                System.out.println("Delapan");
+                break;
+            case 9:
+                System.out.println("Sembilan");
+                break;
+            case 10:
+                System.out.println("Sepuluh");
+                break;
+            default:
+                System.out.println("Invalid number!");
+                break;
+        }
+    }
+}
